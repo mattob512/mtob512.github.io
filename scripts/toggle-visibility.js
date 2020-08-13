d1f7e59116e8c93975118fcb25c9df3d26a2cba3
mtob512 <mtimothyobrien@gmail.com>
2020-08-13 17:42:58 -0400

    Adding tooltips

--- scripts/toggle-visibility.js
+++ scripts/toggle-visibility.js
@@ -2,7 +2,7 @@ function toggleVisibility(id)
 {
     var ele = document.getElementById(id);
     if (ele.style.display === "none" ) {
-      ele.style.display = "inline";
+      ele.show(600);
     }
     else {
       ele.style.display = "none";
