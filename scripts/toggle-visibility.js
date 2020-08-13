747def036b468442f8b5d680b5587dba6691acea
mtob512 <mtimothyobrien@gmail.com>
2020-08-13 17:57:21 -0400

    fixed tooltip issue and moving away from .show for now

--- scripts/toggle-visibility.js
+++ scripts/toggle-visibility.js
@@ -2,7 +2,7 @@ function toggleVisibility(id)
 {
     var ele = document.getElementById(id);
     if (ele.style.display === "none" ) {
-      ele.show(600);
+      ele.style.display = "inline";
     }
     else {
       ele.style.display = "none";
